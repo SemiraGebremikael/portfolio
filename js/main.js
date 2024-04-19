7301ec4311aa742a67cbff04a527f139a53d61e7
Semira gebremikael <semiragere61@gmail.com>
2024-04-19 17:48:58 +0200

    css update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 AOS.init();
 
-// You can also pass an optional settings object
-// below listed default settings
 AOS.init({
-	// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-	offset: 120, // offset (in px) from the original trigger point
-	delay: 0, // values from 0 to 3000, with step 50ms
-	duration: 700, // values from 0 to 3000, with step 50ms
-	easing: "ease", // default easing for AOS animations
-	once: false, // whether animation should happen only once - while scrolling down
-	mirror: false, // whether elements should animate out while scrolling past them
-	anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+	offset: 120,
+	delay: 0,
+	duration: 700,
+	easing: "ease",
+	once: false,
+	mirror: false,
+	anchorPlacement: "top-bottom",
 });
 
 const navbar = document.querySelector(".navbar");
@@ -25,9 +22,9 @@ window.addEventListener("scroll", () => {
 	}
 });
 
-const scriptURL =
-	"https://script.google.com/macros/s/AKfycbya9zxqfiqL3zccU9Fs3mI2g_mhgzsXqbkLpA52Op0i45CNx64x8tZ8pFENeObdFRFm/exec";
-const form = document.forms["web-contact-from"];
+// const scriptURL =
+// 	"https://script.google.com/macros/s/AKfycbya9zxqfiqL3zccU9Fs3mI2g_mhgzsXqbkLpA52Op0i45CNx64x8tZ8pFENeObdFRFm/exec";
+// const form = document.forms["web-contact-from"];
 
 const btnKirim = document.querySelector(".btn-kirim");
 const btnLoading = document.querySelector(".btn-loading");
@@ -35,8 +32,6 @@ const myAlert = document.querySelector(".my-alert");
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	//   ketika tombol  submit diklik
-	// tampilkan  tombol loading, hilangkan tombol kirim
 	btnLoading.classList.toggle("d-none");
 	btnKirim.classList.toggle("d-none");
 
