093758b1999dbd769ba2abc66a968bf3204b1352
semiragebremikael <99189415+Semira-Gebremikael@users.noreply.github.com>
2024-06-13 21:45:46 +0200

    sections and navigation links

--- js/main.js
+++ js/main.js
@@ -10,15 +10,28 @@ AOS.init({
 	anchorPlacement: "top-bottom", //Animation starts when element top is visible, ends when fully shown.
 });
 
-const navbar = document.querySelector(".navbar");
+// Selectors for sections and navigation links
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
 
-window.addEventListener("scroll", () => {
-	if (this.scrollY > 0) {
-		navbar.classList.remove("py-3");
-		navbar.classList.add("shadow", "py-2");
-	} else {
-		navbar.classList.add("py-3");
-		navbar.classList.remove("shadow", "py-2");
+function activemenu() {
+	let currentSectionIndex = sections.length;
+	while (--currentSectionIndex && window.scrollY + 97 < sections[currentSectionIndex].offsetTop) { }
+	navLinks.forEach(link => link.classList.remove("active"));
+	if (navLinks[currentSectionIndex]) {
+		navLinks[currentSectionIndex].classList.add("active");
 	}
-});
+}
+activemenu();
+window.addEventListener("scroll", activemenu);
 
+// links active on click
+document.addEventListener('DOMContentLoaded', function () {
+	const navLinks = document.querySelectorAll('.nav-link');
+	navLinks.forEach(link => {
+		link.addEventListener('click', function () {
+			document.querySelector('.nav-link.active').classList.remove('active');
+			this.classList.add('active');
+		});
+	});
+});
